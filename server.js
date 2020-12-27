621c449de34b3e033d6db3eff4f3fa92c611cacf
Amon Tokoro <amontokoro@amons-mbp-2.home>
2020-12-27 16:02:31 -0500

    init

---
+++ server.js
@@ -0,0 +1,37 @@
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const Post = require('./models/Post');
+const { MONGODB } = require('./config.js');
+
+//q1WHCzp8qJ8LRMza
+
+
+
+const resolvers = {
+    Query: {
+        async getPosts() {
+            try {
+                const posts = await Post.find();
+                return posts;
+            } catch (err) {
+                throw new Error(err);
+            }
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+mongoose.connect(MONGODB, { useNewUrlParser: true})
+    .then(() => {
+        console.log("MongoDB Connceted");
+        return server.listen({ port: 5000 });
+    })
+    .then(res => {
+        console.log(`Server running at ${res.url}`);
+    })
