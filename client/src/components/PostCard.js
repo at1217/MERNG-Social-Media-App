2dcb994a72fb17632c5c065003621b7014ebdd62
Amon Tokoro <amontokoro@amons-mbp-2.home>
2020-12-31 11:38:01 -0500

    displaying posts

---
+++ client/src/components/PostCard.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Card, Icon, Label, Image, Button } from 'semantic-ui-react';
+import moment from 'moment';
+import { Link } from 'react-router-dom';
+
+export const PostCard = ({ post: {body, createdAt, id, username, likeCount, commentCount, likes }}) => {
+
+    const likePost = () => {
+        console.log('Liked Post!');
+    }
+
+    const commentOnPost = () => {
+        console.log('Commented Post!')
+    }
+    return (
+        <Card fluid>
+            <Card.Content>
+                <Image
+                    floated='right'
+                    size='mini'
+                    src='https://react.semantic-ui.com/images/avatar/large/matthew.png'
+                />
+                <Card.Header>{username}</Card.Header>
+                <Card.Meta as={Link} to={`/posts/${id}`}>{moment(createdAt).fromNow()}</Card.Meta>
+                <Card.Description>
+                   {body}
+                </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                <Button as='div' labelPosition='right' onClick={likePost}>
+                    <Button color='teal' basic>
+                        <Icon name='heart' />
+                        Like
+                    </Button>
+                    <Label basic color='teal' pointing='left'>
+                        {likeCount}
+                    </Label>
+                </Button>
+                <Button as='div' labelPosition='right' onClick={commentOnPost}>
+                    <Button color='blue' basic>
+                        <Icon name='comments' />
+                        Comment
+                    </Button>
+                    <Label basic color='blue' pointing='left'>
+                        {commentCount}
+                    </Label>
+                </Button>
+            </Card.Content>
+      </Card>
+    )
+}
+export default PostCard;
